1f10d8eb1c861c8a3e42af4299a1ccd1b01f4cef
AhsanM786 <104119825+AhsanM786@users.noreply.github.com>
2022-04-21 05:25:03 +0100

    Add files via upload

---
+++ 02HelloWorld.py​.py
@@ -0,0 +1,10 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+firstname = input('{;ease enter your firstname? ')
+{;ease enter your firstname? 
+print('Hello ' + firstname)
+ 
+Hello 
+print('Hello Ahsan')
+ 
+Hello Ahsan
