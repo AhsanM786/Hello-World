12c1565b65fd5dcfcd2bcc740f018842ceed0457
AhsanM786 <104119825+AhsanM786@users.noreply.github.com>
2022-04-25 05:06:25 +0100

    Add files via upload

---
+++ Vets 2.py
@@ -0,0 +1,59 @@
+
+
+
+
+obj1 = Antelope()
+#print(obj1.eat())
+
+obj2 = BigFish()
+#print(obj2.eat())
+
+obj3 = LittleFish()
+#print(obj3.eat())
+
+obj4 = Bugs()
+#print(obj4.eat())
+
+obj5 = Bear()
+#print(obj5.eat())
+
+obj6 = Chicken()
+#print(obj6.eat())
+
+obj7 = Cow()
+#print(obj7.eat())
+
+obj8 = Fox()
+#print(obj8.eat())
+
+obj9 = Giraffe()
+#print(obj9.eat())
+
+obj10 = Lion()
+#print(obj10.eat())
+
+obj11 = Panda()
+#print(obj11.eat())
+
+obj12 = Sheep()
+#print(obj12.eat())
+
+obj13 = Leaves()
+
+obj14 = Grass()
+
+
+#Antelope-
+#Big Fish-
+#Little Fish-
+#Bugs-
+#Bear-
+#Chicken-
+#Cow-
+#Sheep-
+#Fox-
+#Giraffe
+#Lion-
+#Panda-
+#Grass-
+#Leaves-
